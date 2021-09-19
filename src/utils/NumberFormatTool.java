f8f362e35078cb5d03cd3656837706857b3aa575
Jeremias Moraes <moraesjeremias@gmail.com>
2021-09-19 18:19:33 -0300

    main - Adding business logic and validation tests

--- src/utils/NumberFormatTool.java
+++ src/utils/NumberFormatTool.java
@@ -2,7 +2,7 @@ package utils;
 
 public class NumberFormatTool {
 
-    public double roundDoubleValue(double doubleValue) {
+    public static double roundDoubleValue(double doubleValue) {
         java.text.NumberFormat numberFormat = java.text.NumberFormat.getInstance();
         numberFormat.setMaximumFractionDigits(2);
         return Double.parseDouble(numberFormat.format(doubleValue));
